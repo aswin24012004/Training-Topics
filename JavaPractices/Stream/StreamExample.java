eceaad9aaf409742c2247ae7a05f7c30e04abc3d
Aswin_B.E_CSE <141166978+aswin24012004@users.noreply.github.com>
2026-01-31 07:15:30 +0000

    stream basic code is added

---
+++ JavaPractices/Stream/StreamExample.java
@@ -0,0 +1,18 @@
+package practices.streams;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamExample {
+	public static void main(String args[]) {
+		
+		List<String> names = Arrays.asList("Aswin", "Raj", "Anu", "Arun");
+		
+		List<String> result = names.stream()
+				.filter(name -> name.startsWith("A"))
+				.map(String :: toUpperCase)
+				.collect(Collectors.toList());
+		
+		System.out.println(result);
+	}
+}
