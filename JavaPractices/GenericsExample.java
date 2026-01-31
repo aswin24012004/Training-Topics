14b4390d2d91295ccf0c6dc8a19cadaacc94dc8f
Aswin_B.E_CSE <141166978+aswin24012004@users.noreply.github.com>
2026-01-31 07:02:15 +0000

    Generic file is added

---
+++ JavaPractices/GenericsExample.java
@@ -0,0 +1,29 @@
+package practices;
+
+public class GenericsExample {
+
+    static class Box<T> {
+
+        private T value;
+
+        public void set(T value) {
+            this.value = value;
+        }
+
+        public T get() {
+            return value;
+        }
+    }
+
+    public static void main(String args[]) {
+    	
+        Box<Integer> intBox = new Box<>();
+        intBox.set(10);
+        System.out.println(intBox.get());
+       
+        
+        Box<String> strBox = new Box<>();
+        strBox.set("Hello");
+        System.out.println(strBox.get());
+    }
+}
